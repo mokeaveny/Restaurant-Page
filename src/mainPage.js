f3b910c22e056e2420a1ff4b1eac2c52cada1fae
mokeaveny <michaelkeaveny0@gmail.com>
2020-06-03 21:54:04 +0100

    Added a bit of styling to the webpage

--- src/mainPage.js
+++ src/mainPage.js
@@ -3,12 +3,15 @@ import { contentContainer } from "./index";
 export function createMainPage() {
 	const navBar = document.createElement("nav");
 	const heading = document.createElement("h1");
+	const menuButton = document.createElement("button");
+	const contactButton = document.createElement("button");
+	menuButton.textContent = "Menu";
+	contactButton.textContent = "Contact";
 	heading.textContent = "Mo's Magic Munch";
 	navBar.appendChild(heading);
+	navBar.appendChild(menuButton);
+	navBar.appendChild(contactButton);
 	contentContainer.appendChild(navBar);
-	const restaurantImage = document.createElement("img");
-	restaurantImage.src = "assets/restaurant.jpg";
-	contentContainer.appendChild(restaurantImage);
 	const restaurantInfo = document.createElement("p");
 	restaurantInfo.textContent = "The best up and coming restaurant in the U.K. Choose from a selection of innovative dishes that will completely blow you away!";
 	contentContainer.appendChild(restaurantInfo);
