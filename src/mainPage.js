6d248028b9a9057494e1e6155a164b33da016fd9
mokeaveny <michaelkeaveny0@gmail.com>
2020-06-04 11:41:40 +0100

    Updated Styling

--- src/mainPage.js
+++ src/mainPage.js
@@ -3,15 +3,20 @@ import { contentContainer } from "./index";
 export function createMainPage() {
 	const navBar = document.createElement("nav");
 	const heading = document.createElement("h1");
+	const buttonContainer = document.createElement("div")
+	buttonContainer.id = "button-container";
 	const menuButton = document.createElement("button");
 	const contactButton = document.createElement("button");
 	menuButton.textContent = "Menu";
 	contactButton.textContent = "Contact";
 	heading.textContent = "Mo's Magic Munch";
+
+	buttonContainer.appendChild(menuButton);
+	buttonContainer.appendChild(contactButton);
 	navBar.appendChild(heading);
-	navBar.appendChild(menuButton);
-	navBar.appendChild(contactButton);
+	navBar.appendChild(buttonContainer);
 	contentContainer.appendChild(navBar);
+
 	const restaurantInfo = document.createElement("p");
 	restaurantInfo.textContent = "The best up and coming restaurant in the U.K. Choose from a selection of innovative dishes that will completely blow you away!";
 	contentContainer.appendChild(restaurantInfo);
