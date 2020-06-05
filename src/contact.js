dff3e4f0e1736e19ea30c4468d1ff3c585a3cc11
mokeaveny <michaelkeaveny0@gmail.com>
2020-06-05 19:11:30 +0100

    Finished Project

--- src/contact.js
+++ src/contact.js
@@ -2,9 +2,33 @@ export function renderContact() {
 	const contactContainer = document.createElement("div");
 	contactContainer.id = "current-container";
 	const contactHeader = document.createElement("h1");
+	contactHeader.id = "contact-header";
 	contactHeader.textContent = "Contact Mo Keavo";
+	const paraInfo = document.createElement("p");
+	paraInfo.textContent = "Email: mike@momunch.com"
+	const paraBreak = document.createElement("br");
+	const addressLines = document.createElement("ul");
+	addressLines.id = "address-lines"
+	const addressHeader = document.createElement("h2");
+	addressHeader.textContent = "Address:";
+	const addressLine1 = document.createElement("li");
+	addressLine1.textContent = "Mo's Magic Munch"
+	const addressLine2 = document.createElement("li");
+	addressLine2.textContent = "56 Delicious Lane"
+	const addressLine3 = document.createElement("li");
+	addressLine3.textContent = "Flavour City"
+	const addressLine4 = document.createElement("li");
+	addressLine3.textContent = "Planet Earth";
 
 	contactContainer.appendChild(contactHeader);	
+	paraInfo.appendChild(paraBreak);
+	addressLines.appendChild(addressHeader);
+	addressLines.appendChild(addressLine1);
+	addressLines.appendChild(addressLine2);
+	addressLines.appendChild(addressLine3);
+	addressLines.appendChild(addressLine4);
+	paraInfo.appendChild(addressLines);
+	contactContainer.appendChild(paraInfo);
 
 	return contactContainer;
 }
