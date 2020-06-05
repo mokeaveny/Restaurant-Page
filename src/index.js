723460c04f5a69e3e3045da69dd77211506c9326
mokeaveny <michaelkeaveny0@gmail.com>
2020-06-05 18:04:44 +0100

    Added a button that displays the general info about the restaurant

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createMainPage } from "./mainPage";
 import { renderMenu } from "./menu";
+import { renderAboutText } from "./mainPage";
 import { renderContact } from "./contact";
 
 export const contentContainer = document.getElementById("content");
@@ -7,11 +8,16 @@ export const contentContainer = document.getElementById("content");
 // Render the main page from the mainPage.js file
 createMainPage();
 
+const aboutButton = document.getElementById("about-button");
 const menuButton = document.getElementById("menu-button");
 const contactButton = document.getElementById("contact-button");
-const aboutText = document.getElementById("restaurant-info");
-aboutText.id = "current-container";
 
+aboutButton.addEventListener("click", () => {
+	const aboutTextContainer = renderAboutText();
+	const currentContainer = document.getElementById("current-container");
+	contentContainer.removeChild(currentContainer);
+	contentContainer.appendChild(aboutTextContainer);
+});
 
 // When the menu utton is clicked then the elements created by renderMenu() are assigned to the menuContainer variable and appended to the contentContainer.
 menuButton.addEventListener("click", () => {
--- src/mainPage.js
+++ src/mainPage.js
@@ -5,23 +5,33 @@ export function createMainPage() {
 	const heading = document.createElement("h1");
 	const buttonContainer = document.createElement("div")
 	buttonContainer.id = "button-container";
+	const aboutButton = document.createElement("button");
 	const menuButton = document.createElement("button");
 	const contactButton = document.createElement("button");
+	aboutButton.textContent = "About";
+	aboutButton.id = "about-button";
 	menuButton.id = "menu-button";
 	menuButton.textContent = "Menu";
 	contactButton.textContent = "Contact";
 	contactButton.id = "contact-button";
 	heading.textContent = "Mo's Magic Munch";
 
+	buttonContainer.appendChild(aboutButton);
 	buttonContainer.appendChild(menuButton);
 	buttonContainer.appendChild(contactButton);
 	navBar.appendChild(heading);
 	navBar.appendChild(buttonContainer);
 	contentContainer.appendChild(navBar);
 
-	const restaurantInfo = document.createElement("p");
-	restaurantInfo.id = "restaurant-info";
-	restaurantInfo.textContent = "The best up and coming restaurant in the U.K. Choose from a selection of innovative dishes that will completely blow you away!";
+	const restaurantInfo = renderAboutText();
+
 	contentContainer.appendChild(restaurantInfo);
+}
+
+export function renderAboutText() {
+	const restaurantInfo = document.createElement("p");
+	restaurantInfo.id = "current-container";
+	restaurantInfo.textContent = "The best up and coming restaurant in the U.K. Choose from a selection of innovative dishes that will completely blow you away!";
+	return restaurantInfo;
 }
 
